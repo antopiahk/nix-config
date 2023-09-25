913ac5c544587b11d92f6cd903c42accde1543f5
Jorge Lewis <31695089+antopiahk@users.noreply.github.com>
2023-09-25 20:33:10 +0800

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -21,8 +21,8 @@
   i18n = {
     defaultLocale = "en_US.UTF-8";
     extraLocaleSettings = {
-      LC_ALL="en_US.UTF-8"
-      LC_CTYPE="en_US.UTF-8"
+      LC_ALL= "en_US.UTF-8";
+      LC_CTYPE= "en_US.UTF-8";
     };
 
   };
