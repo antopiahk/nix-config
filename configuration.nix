ff3612fb789352bff823a492e25c3752266e6f59
Jorge Lewis <31695089+antopiahk@users.noreply.github.com>
2023-09-25 20:23:14 +0800

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -20,6 +20,10 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
+  i18n.extraLocaleSettings = {
+    LC_ALL= "en_US.UTF-8";
+  };
+
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
